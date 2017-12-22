b272031b97e69d0262528c2492d17346e5983fa9
owen ekhator <oe52@afsconnect1.njit.edu>
2017-12-22 17:22:28 -0500

    deleted unused files

--- core/utility/validation.php
+++ core/utility/validation.php
@@ -7,23 +7,33 @@ class validation
 
     static function password($data)
     {
-        if(!count($data)>=6)
-        {
+        if (!count($data) >= 6) {
             return false;
-        }
-        else
-        {
+        } else {
             return true;
         }
 
     }
 
+    static function field($data)
+    {
+        if (!count($data) >= 1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 
+
+    static function email($email)
+    {
+
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 
 }
-
-
-
-
-?>
